4fab322b2d6a5f92fbe23f08971bf74ac81c294a
Peter Goodman <peter.goodman@gmail.com>
2011-11-24 17:14:35 -0500

    Little change for reinterpret_cast.

--- xy/lib/utf8/codepoint.cpp
+++ xy/lib/utf8/codepoint.cpp
@@ -21,7 +21,7 @@ namespace xy { namespace utf8 {
     }
 
     bool operator==(const char c, const codepoint &cp) throw() {
-        return reinterpret_cast<uint8_t>(c) == cp.data[0] && '\0' == cp.data[1];
+        return c == reinterpret_cast<const char *>(cp.data)[0] && '\0' == cp.data[1];
     }
 
     bool codepoint::operator==(const codepoint &o) const throw() {
